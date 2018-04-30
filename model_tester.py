8961b6c95239e264e7915279d4a0c121556b6d67
Bobby <Bobby-Wu@web.de>
2018-04-30 09:50:46 +0200

    Training_2

--- model_tester.py
+++ model_tester.py
@@ -7,21 +7,29 @@ Created on Fri Apr 27 20:55:03 2018
 """
 import numpy as np
 import tensorflow as tf
+#import model_trainer as mt
+from model_trainer import get_mfcc , recurrent_neural_network
 
-from model_trainer import get_mfcc
 
+tf.reset_default_graph()
+
+# batch_Size, Sequence_length, n_mfcc
+x = tf.placeholder(tf.float32, [None, None, 40])
+# batch_Size, Sequence_length_labels
+y = tf.placeholder(tf.float32, [None, None,2])
+
+
+sess = tf.Session()
 saver = tf.train.Saver()
-with tf.session as sess:
-    
-    saver.restore(sess, "models/model.ckpt")
-    
-    y_prediction = [];
-    input_data = get_mfcc("Test_Data/142_slices/142_part_1")
-    input_x = input_data.reshape((1,input_data.shape[0],input_data.shape[1]))
-    
-    sess.run(y_prediction, feed_dict={x:input_x})
-        
+saver.restore(sess, "models/model.ckpt")
+prediction = recurrent_neural_network()
 
+input_data = get_mfcc("Test_Data/142_slices/142_part_1")
+input_x = input_data.reshape((1,input_data.shape[0],input_data.shape[1]))
+    
+output_y = sess.run(prediction, feed_dict={x:input_x})
+        
+print(output_y)
 
 
 
--- model_trainer.py
+++ model_trainer.py
@@ -128,14 +128,17 @@ def prepare_data(folder):
     train_data = [(x_data[i],y_data[i]) for i in range(0, len(x_data))]
     x_train = np.asarray([pair[0] for pair in train_data])
     y_train = np.asarray([pair[1] for pair in train_data])
+    return x_train,y_train    
     
-    return x_train,y_train
+#    x_input = [s.reshape((1,s.shape[0],s.shape[1])) for s in x_train]
+#    y_input = [s.reshape((1,s.shape[0],s.shape[1])) for s in y_train]
+#    return x_input, y_input
 
     
 # batch_Size, Sequence_length, n_mfcc
-x = tf.placeholder(tf.float32, [None, None, n_mfcc])
+x = tf.placeholder(tf.float32, [None, None, n_mfcc], name = "x")
 # batch_Size, Sequence_length_labels
-y = tf.placeholder(tf.float32, [None, None,2])
+y = tf.placeholder(tf.float32, [None, None,2], name = "y")
 
 rnn_size = 512
 
@@ -151,25 +154,22 @@ def recurrent_neural_network():
  
     outputs, states = rnn.dynamic_rnn(lstm_cell, x, dtype=tf.float32)
     output = tf.matmul(outputs[-1], layer['weights']) + layer['biases']
- 
-    print("out_put_shape: ")
-    print(output.shape)
-    
+
     return output
 
-def train_neural_network(learning_rate = 0.01, batch_size=1 ,hm_epochs=100):
+def train_neural_network(learning_rate = 0.01, batch_size=1 ,hm_epochs=500):
     
     
     x_data, y_data = prepare_data("Wave_sliced")
-    
+    x_test ,y_test = prepare_data("Test_Data")
 #    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data)
-         
+
     prediction = recurrent_neural_network()
-    # oder netz ist nicht mit der loss funktion verbunden
-    ## sigmoid konstante C um 0 und 1 auszugleichen 
+
+# sigmoid konstante C um 0 und 1 auszugleichen     
+# oder netz ist nicht mit der loss funktion verbunden
     
     cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels = y[-1], logits = prediction))
- 
     optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
     
     saver = tf.train.Saver();
@@ -179,63 +179,71 @@ def train_neural_network(learning_rate = 0.01, batch_size=1 ,hm_epochs=100):
                 
 #        train_dict = {x: x_data, y: y_data}
 #        test_dict = {x: x_test, y: y_test}
-        
-#        print("Initial training loss: " + str(sess.run(cost, train_dict)))
-#        print("Initial test loss: " + str(sess.run(cost, test_dict)))
-        
+    
         for epoch in range(hm_epochs):
             epoch_loss = 0
             
             epoch_data = list(zip(x_data, y_data))
             np.random.shuffle(epoch_data)
             
-#            _, c= sess.run([optimizer, cost], feed_dict= train_dict)
-            
-            for x_sample,y_sample in epoch_data:
-#                epoch_x, epoch_y = speech_data.next_batch(batch_size)
-                    
-                 
-                epoch_x = x_sample.reshape((1,x_sample.shape[0],x_sample.shape[1]))
-                
-                epoch_y = y_sample.reshape((1,y_sample.shape[0],y_sample.shape[1]))
-                
-                
-                
+            for x_sample,y_sample in epoch_data:                 
+                epoch_x = x_sample.reshape((1,x_sample.shape[0],x_sample.shape[1]))                
+                epoch_y = y_sample.reshape((1,y_sample.shape[0],y_sample.shape[1]))                                                
                 _, c= sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
-                epoch_loss += c
-                
-                
+                epoch_loss += c                
             average_loss = epoch_loss / len(epoch_data)
             print('Epoch', str(epoch), 'completed out of',hm_epochs,
                   'loss:', str(epoch_loss),
                   'average loss', str(average_loss))
         
-        save_path = saver.save(sess, "models/model.ckpt")
+        save_path = saver.save(sess, "models/model.ckpt")        
+        print("Model saved in path: %s" % save_path)        
+#        saver.restore(sess,"saved_models/model.ckpt")        
         
-        print("Model saved in path: %s" % save_path)
+        test_data = list(zip(x_test, y_test))
+        
+        hit = 0 # true positive
+        miss = 0 # false negative
+        false_alarm = 0 # false positive
+        correct_rejection = 0  # true negative
+        
+        for x_sample,y_sample in test_data:
+            
+            y_prediction = sess.run(prediction, feed_dict={x:x_sample.reshape((1,x_sample.shape[0],x_sample.shape[1]))})
+            y_prediction = np.argmax(y_prediction,1)
+            y_labeled = np.argmax(y_sample,1)
+            
+            correct = np.equal(y_prediction,y_labeled)
+            
+            for i in range(len(correct)):
+                if correct[i]:
+                    if y_labeled[i] == 1:
+                        hit = hit + 1
+                    else: correct_rejection  = correct_rejection + 1
+                else:
+                    if y_labeled[i] == 1:
+                        miss = miss + 1
+                    else: false_alarm = false_alarm + 1
+                    
+        total_amount = hit + correct_rejection + miss + false_alarm            
+        accuracy = float((hit + correct_rejection))/total_amount
+        print("Accuracy: ",accuracy)
 
-#        
-#        saver.restore(sess,"models/model.ckpt")
-        
-        
-        test_x = get_mfcc("Test_Data/142_slices/142_part_1") 
-        test_y = get_label("Test_Data/142_slices/142_part_1", 142)
-        test_x = test_x.reshape((1,test_x.shape[0],test_x.shape[1]))
-        
-        y_prediction = sess.run(prediction, feed_dict={x: test_x})
-        print(y_prediction) 
-        y_prediction = np.argmax(y_prediction, 1)
-        y_labeled = np.argmax(test_y,1)
-        print(y_prediction) 
-        print(y_labeled)     
-        
-        
-#        correct = tf.equal(y_prediction,y_labeled)
+#        test_x = get_mfcc("Test_Data/142_slices/142_part_1") 
+#        test_y = get_label("Test_Data/142_slices/142_part_1", "142")
+#        test_x = test_x.reshape((1,test_x.shape[0],test_x.shape[1]))        
+#        y_prediction = sess.run(prediction, feed_dict={x: test_x})
+##        print(y_prediction) # roh_daten zum auswerten von der Klassefizierung        
+#        y_prediction = np.argmax(y_prediction, 1)
+#        y_labeled = np.argmax(test_y,1)
+##        print(y_prediction) 
+##        print(y_labeled)
+#        correct = np.equal(y_prediction,y_labeled)
+##        correct = tf.equal(y_prediction,y_labeled)
 #        print(correct)
-#        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-#
 #        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-#        
+#        print(accuracy)
+        
 #        print('Accuracy:',accuracy.eval(train_dict))
 
 train_neural_network()
